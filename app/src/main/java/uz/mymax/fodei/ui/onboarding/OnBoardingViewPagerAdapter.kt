09e03fc40258b2e2167f93dd462ad672212a88e4
Elyor Keldiyorov <Elyor6336>
2021-12-21 10:14:37 +0500

    Better version of veiwPager

--- app/src/main/java/uz/mymax/fodei/ui/onboarding/OnBoardingViewPagerAdapter.kt
+++ app/src/main/java/uz/mymax/fodei/ui/onboarding/OnBoardingViewPagerAdapter.kt
@@ -1,28 +1,35 @@
 package uz.mymax.fodei.ui.onboarding
 
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import uz.mymax.fodei.R
 
-class OnBoardingViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
+class OnBoardingViewPagerAdapter(fm: FragmentManager, lifecycle: Lifecycle, skip: TextView) :
     FragmentStateAdapter(fm, lifecycle) {
     override fun getItemCount() = 3
-
+    val skip_buuton = skip
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
+        when (position) {
             0 -> {
-                NearbyOnboardingFragmant.newInstance()
+                skip_buuton.visibility = View.VISIBLE
+                return NearbyOnboardingFragmant.newInstance()
             }
             1 -> {
-                FavoriteOnboardingFragmant.newInstance()
+                skip_buuton.visibility = View.VISIBLE
+                return FavoriteOnboardingFragmant.newInstance()
             }
             2 -> {
-                CheapOnboardingFragmant.newInstance()
+                skip_buuton.visibility = View.INVISIBLE
+                return CheapOnboardingFragmant.newInstance()
             }
 
             else -> {
-                NearbyOnboardingFragmant.newInstance()
+                skip_buuton.visibility = View.VISIBLE
+                return NearbyOnboardingFragmant.newInstance()
             }
         }
 
--- app/src/main/java/uz/mymax/fodei/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/uz/mymax/fodei/ui/onboarding/OnboardingFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.rd.PageIndicatorView
 import uz.mymax.fodei.R
 
 
@@ -26,10 +35,16 @@ class OnboardingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<ImageView>(R.id.next)
+        val vpAdapter = OnBoardingViewPagerAdapter(parentFragmentManager, lifecycle, (view.findViewById(R.id.skip)))
+        val viewPager = view.findViewById<ViewPager2>(R.id.vpOnboarding)
+        viewPager.adapter= vpAdapter
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
 
-        val vpAdapter = OnBoardingViewPagerAdapter(parentFragmentManager, lifecycle)
-        view.findViewById<ViewPager2>(R.id.vpOnboarding).adapter=vpAdapter
-
-
+        }.attach()
+        button.setOnClickListener {
+            viewPager.currentItem = viewPager.currentItem+1
+        }
     }
 }
